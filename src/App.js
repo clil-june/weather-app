69623c891d9cdf17a249afb42a2808e3bee68af5
Clil SneOR <clil.s.au@gmail.com>
2021-03-18 11:44:53 +0200

    put all my component in one content component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 
 import './App.css';
-import SearchBar from "./SearchBar";
-import LocationButton from "./LocationButton"
-import City from "./City";
-import Temperature from "./Temperature";
-import Description from "./Description";
-import Time from "./Time"
-import Iformation from "./Information";
+import Content from "./Content"
 import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <div className="project">
-      <SearchBar />
-      <LocationButton />
-      <City />
-      <Temperature />
-      <Description />
-      <Time />
-      <Iformation />
+      <Content />
       </div>
       <Footer />
     </div>
---
+++ src/Content.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Time from "./Time";
+import "./Content.css";
+
+export default function Content (){
+    return (
+        <div className="content">
+      <form autocomplete="off" id="search-form" >
+      <input id="search-bar" type="search" placeholder="Enter city" />
+      <input id="search" type="submit" value="search" />
+     </form> 
+
+     <button id="your-location">
+        <i class="fas fa-map-marker-alt"></i>
+      </button>
+
+      <h1 id="location">
+        Sde Boker
+      </h1>
+
+      <h2>
+      <span id="temperature">16</span> <small class="unit"><a href="/" id="celsius">°C</a> |<a href="/" id="fahrenheit"> °F</a></small>
+      </h2>
+
+      <div className="Description">
+    <section id="dicription">Cloudy</section>
+    <img src="https://ssl.gstatic.com/onebox/weather/48/cloudy.png" alt="weather icon" id="icon" />
+</div>
+
+<Time />
+
+<p>
+        <div id="wind"><i class="fas fa-wind"></i> Wind: 37 km/h</div>
+        <div id="humidity"><i class="fas fa-tint"></i> Humidity: 54%</div>
+      </p>
+     </div> 
+    );
+}
